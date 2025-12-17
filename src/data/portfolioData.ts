32ff7b60a8f8480db70eb1d5a9a1c6143cb5997b
risyalf <risyalfebrianto97@gmail.com>
2025-12-17 17:55:54 +0700

    update foto

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -22,9 +22,38 @@ export const projects: Project[] = [
     summary: "Develop e-commerce website with user-friendly interface.",
     link: "https://listromart.com",
     images: [
-      "/experience/ecommerce1.png",
+      "/experience/0.png",
+      "/experience/6.png",
+      "/experience/1.jpeg",
+      "/experience/2.jpeg",
+      "/experience/3.jpeg",
+      "/experience/4.jpeg",
+      "/experience/5.jpeg",
     ],
     tags: ["E-Commerce", "MySQL", "User Friendly"],
+    details: {
+      challenge: "The company needed a user-friendly e-commerce platform to enhance customer experience and streamline online sales.",
+      solution: "Developed a responsive e-commerce website using modern web technologies, ensuring seamless navigation and efficient checkout processes.",
+      impact: [
+        "Increased online sales by 30% within the first quarter after launch.",
+        "Received positive feedback from customers regarding the website's usability.",
+        "Reduced cart abandonment rates by 15% through optimized checkout flow."
+      ]
+    }
+  },
+  {
+    id: 1,
+    title: "Attendance Website Development",
+    summary: "Develop attendace website with user-friendly interface.",
+    link: "https://absen.listromart.com",
+    images: [
+      "/experience/absen_1.png",
+      "/experience/absen_2.png",
+      "/experience/absen_3.png",
+      "/experience/absen_4.png",
+      "/experience/absen_5.png",
+    ],
+    tags: ["Attendance", "MySQL", "User Friendly"],
     details: {
       challenge: "The company needed a user-friendly e-commerce platform to enhance customer experience and streamline online sales.",
       solution: "Developed a responsive e-commerce website using modern web technologies, ensuring seamless navigation and efficient checkout processes.",
